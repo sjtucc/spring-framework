5f7ff017875a69c22ab7620a4690d0dfa62cfe56
CHEN <1535812566@qq.com>
2019-10-25 08:56:10 +0800

    edit

--- spring-beans/src/main/java/org/springframework/beans/factory/support/ConstructorResolver.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/ConstructorResolver.java
@@ -138,7 +138,6 @@ class ConstructorResolver {
 				}
 			}
 
-            // TODO 芋艿 创建 Bean
             if (candidates.length == 1 && explicitArgs == null && !mbd.hasConstructorArgumentValues()) {
 				Constructor<?> uniqueCandidate = candidates[0];
 				if (uniqueCandidate.getParameterCount() == 0) {
